abc5e171fc4a70ccad24886146d7b236a4202281
Your Name <test>
2021-02-27 11:26:33 +0800

    fix

--- App.py
+++ App.py
@@ -1,32 +1,9 @@
-
-# In[8]:
-
-
 import streamlit as st
-# try:
-#     from IPython.display import display
-#     st.write = display
-#     st.write('is running on notebook')
-# except:
-#     st.write('is running on streamlit')
-
-
-# In[9]:
-
-
 import requests
-r = requests.get('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes')
-
-# In[10]:
 
 if st.button('Joke?'):
+    r = requests.get('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes')
     st.write(f'''{r.json()['setup']}
     {r.json()['punchline']}
     ''')
-
-
-# In[ ]:
-
-
-
 
