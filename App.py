4e7840df43d6c3ad69db27f0f2b6776c7f3ced14
Your Name <test>
2021-02-27 11:29:39 +0800

    fix

--- App.py
+++ App.py
@@ -2,8 +2,8 @@ import streamlit as st
 import requests
 
 if st.button('Joke?'):
-    r = requests.get('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes')
-    st.write(f'''{r.json()['setup']}
-    {r.json()['punchline']}
+    r = requests.get('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/type/general/1')
+    st.write(f'''{r.json()[0]['setup']}
+    {r.json()[0]['punchline']}
     ''')
 
